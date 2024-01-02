d53ce8f5d0216c728f5fb0c2ef5fb707d4888f5c
Маршалл <44672093+LunaTMT@users.noreply.github.com>
2024-01-02 21:54:55 +0000

    Add files via upload

--- entities/enemy.py
+++ entities/enemy.py
@@ -9,4 +9,10 @@ class Enemy(pygame.sprite.Sprite):
         self.grid = game.grid
         self.x, self.y = start_position
     
-    
+        self.behaviour = ("CHASE", "SCATTER", "FRIGHTENED")
+        """
+           Blinky (red) is very aggressive and hard to shake once he gets behind you, 
+           Pinky  (pink) tends to get in front of you and cut you off, 
+           Inky   (light blue) is the least predictable of the bunch, 
+           Clyde  (orange) seems to do his own thing and stay out of the way.
+        """
